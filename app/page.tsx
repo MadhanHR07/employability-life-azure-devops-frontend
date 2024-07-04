f326fc87c48dba0a1618f67ef12100fcb5f8bc5e
kathariasumit <sumit.katharia@asts.technology>
2024-07-04 21:18:34 +0530

    04-July-2024

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import Head from 'next/head';
-import HelloWorld from '@/components/HelloWorld';
+"use client";
+import * as React from "react";
+import { PaletteMode } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
+import Hero from "../components/Hero";
+import getLPTheme from "./getLPTheme";
+import Head from "next/head";
+import Login from "@/components/Login";
+
+interface ToggleCustomThemeProps {
+  showCustomTheme: Boolean;
+  toggleCustomTheme: () => void;
+}
 
 const Home = () => {
+  const [mode, setMode] = React.useState<PaletteMode>("light");
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+  const [isLogin, setIsLogin] = React.useState(false);
   return (
     <div>
       <Head>
@@ -9,10 +30,10 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>Welcome to Next.js!</h1>
-        <HelloWorld />
-      </main>
+      <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+        <CssBaseline />
+        {isLogin ? <Hero /> : <Login setIsLogin={setIsLogin} />}
+      </ThemeProvider>
     </div>
   );
 };
